47dc28d562245494c95db3bca47fd648aa3da575
TheHDeveloper <yash160203@gmail.com>
2020-01-29 22:13:00 +0530

    commented line 4

--- fibonacci.c
+++ fibonacci.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void fib(int n)
 {   
-    int a,s=0;
+    int a,s=0;//a=next number in series after first,s=previous number in series number in series
     for(int i=1;i<=n;i++)
     {
         if(i==1)
