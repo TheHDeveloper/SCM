e042b873f1d9abcc9f20a5917a81ca0a4d433607
TheHDeveloper <yash160203@gmail.com>
2020-02-04 16:59:49 +0530

    commented line 7

--- fibonacci.c
+++ fibonacci.c
@@ -4,7 +4,7 @@ void fib(int n)
     int a,s=0;//a=next number in series after first,s=previous number in series number in series
     for(int i=1;i<=n;i++)
     {
-        if(i==1)
+        if(i==1)//for i = 1,there is no previous value for fibonacci series
         {
             a=0;
             printf("%d",i);
